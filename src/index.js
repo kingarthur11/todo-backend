e18b3281ce942fcde602d3e279f3d4dffd6b0ade
kingarthur11 <orduharthur@gmail.com>
2024-03-24 23:16:35 +0100

    test server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const app = require('./config/express');
 // const mongoose = require('./config/mongoose');
 
 // // mongoose.connect();
-// app.listen(3000, () => console.log("Server ready on port 3000."));
 
 // module.exports = app;
 
