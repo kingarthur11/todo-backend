56fe2c2a1452fd1ec59adc3cc74c9a5601134f68
kingarthur11 <orduharthur@gmail.com>
2024-03-24 22:49:22 +0100

    test server

--- src/config/express.js
+++ src/config/express.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const httpStatus = require('http-status');
-const cors = require('cors');
-const routes = require('../api/routes/v1');
-const { errorHandler } = require('../api/middleware/error');
-const ApiError = require('../api/utils/ApiError');
+// const cors = require('cors');
+// const routes = require('../api/routes/v1');
+// const { errorHandler } = require('../api/middleware/error');
+// const ApiError = require('../api/utils/ApiError');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
-app.use(express.static(__dirname+'/public'));
+// app.use(express.static(__dirname+'/public'));
 
-app.use('/api', routes);
+// app.use('/api', routes);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Welcome to my server!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Welcome to my server!');
+// });
 
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
