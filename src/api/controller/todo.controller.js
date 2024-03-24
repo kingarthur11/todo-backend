19fd20ae241707fee0f5c6a33103ced8b0259916
kingarthur11 <orduharthur@gmail.com>
2024-03-25 00:14:43 +0100

    update the server error

--- src/api/controller/todo.controller.js
+++ src/api/controller/todo.controller.js
@@ -1,41 +1,66 @@
 const httpStatus = require("http-status");
-const catchAsync = require("../utils/catchAsync");
-const { todoService } = require("../services");
+// const catchAsync = require("../utils/catchAsync");
+// const { todoService } = require("../services");
 
-const createTodo = catchAsync(async (req, res) => {
-  // const todo = await todoService.createTodo(req.body);
-  // res.status(httpStatus.CREATED).json({ message: "Created successfully", todo });
-});
+const createTodo = async (req, res) => {
+  return "hello world"
+}
 
-const getAllTodo = catchAsync(async (req, res) => {
-  // const todo = await todoService.getAllTodos();
-  // res.status(httpStatus.OK).json({ message: "Todo retrieved successfully", todo });
-});
+const getAllTodo = async (req, res) => {
+  return "hello world"
+}
+const filterTodosByStatus = async (req, res) => {
+  return "hello world"
+}
+const getOneTodo = async (req, res) => {
+  return "hello world"
+}
+const updateTodo = async (req, res) => {
+  return "hello world"
+}
 
-const filterTodosByStatus = catchAsync(async (req, res) => {
-  // const todo = await todoService.filterTodosByStatus(req.params.status);
-  // res.status(httpStatus.OK).json({ message: "Todo retrieved successfully", todo });
-});
+const updateTodoStatus = async (req, res) => {
+  return "hello world"
+}
 
-const getOneTodo = catchAsync(async (req, res) => {
-  // const todo = await todoService.getTodoById(req.params.todo_id);
-  // res.status(httpStatus.OK).json({ message: "Todo retrieved successfully", todo });
-});
+const deleteTodo = async (req, res) => {
+  return "hello world"
+}
 
-const updateTodo = catchAsync(async (req, res) => {
-  // const todo = await todoService.updateTodo(req.params.todo_id, req.body);
-  // res.status(httpStatus.OK).json({ message: "Todo updated successfully", todo });
-});
+// const createTodo = catchAsync(async (req, res) => {
+//   const todo = await todoService.createTodo(req.body);
+//   res.status(httpStatus.CREATED).json({ message: "Created successfully", todo });
+// });
 
-const updateTodoStatus = catchAsync(async (req, res) => {
-  // const todo = await todoService.updateTodoStatus(req.params.todo_id, req.body);
-  // res.status(httpStatus.OK).json({ message: "Todo status updated successfully", todo });
-});
+// const getAllTodo = catchAsync(async (req, res) => {
+//   const todo = await todoService.getAllTodos();
+//   res.status(httpStatus.OK).json({ message: "Todo retrieved successfully", todo });
+// });
 
-const deleteTodo = catchAsync(async (req, res) => {
-  // await todoService.deleteTodo(req.params.todo_id);
-  // res.json({ message: "Deleted successfully" });
-});
+// const filterTodosByStatus = catchAsync(async (req, res) => {
+//   const todo = await todoService.filterTodosByStatus(req.params.status);
+//   res.status(httpStatus.OK).json({ message: "Todo retrieved successfully", todo });
+// });
+
+// const getOneTodo = catchAsync(async (req, res) => {
+//   const todo = await todoService.getTodoById(req.params.todo_id);
+//   res.status(httpStatus.OK).json({ message: "Todo retrieved successfully", todo });
+// });
+
+// const updateTodo = catchAsync(async (req, res) => {
+//   const todo = await todoService.updateTodo(req.params.todo_id, req.body);
+//   res.status(httpStatus.OK).json({ message: "Todo updated successfully", todo });
+// });
+
+// const updateTodoStatus = catchAsync(async (req, res) => {
+//   const todo = await todoService.updateTodoStatus(req.params.todo_id, req.body);
+//   res.status(httpStatus.OK).json({ message: "Todo status updated successfully", todo });
+// });
+
+// const deleteTodo = catchAsync(async (req, res) => {
+//   await todoService.deleteTodo(req.params.todo_id);
+//   res.json({ message: "Deleted successfully" });
+// });
 
 module.exports = {
   createTodo,
