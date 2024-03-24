b98b3d0a62d733236acfdccd4ad9c8036292afac
kingarthur11 <orduharthur@gmail.com>
2024-03-24 23:13:04 +0100

    test server

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ const app = require('./config/express');
 
 // module.exports = app;
 
-// const express = require("express");
-// const app = express();
-
 // app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
