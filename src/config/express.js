a81704e30b6a680e0596c1eb1a481e443cba5aa0
kingarthur11 <orduharthur@gmail.com>
2024-03-24 22:58:08 +0100

    test server

--- src/config/express.js
+++ src/config/express.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const httpStatus = require('http-status');
 const cors = require('cors');
-// const routes = require('../api/routes/v1');
-// const { errorHandler } = require('../api/middleware/error');
-// const ApiError = require('../api/utils/ApiError');
+const routes = require('../api/routes/v1');
+const { errorHandler } = require('../api/middleware/error');
+const ApiError = require('../api/utils/ApiError');
 
 const app = express();
 
@@ -12,11 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 app.use(express.static(__dirname+'/public'));
 
-// app.use('/api', routes);
+app.use('/api', routes);
 
-// app.get('/', (req, res) => {
-//   res.send('Welcome to my server!');
-// });
+app.get('/', (req, res) => {
+  res.send('Welcome to my server!');
+});
 
 // app.use((req, res, next) => {
 //   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = require('./config/express');
 // const express = require("express");
 // const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+// app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
